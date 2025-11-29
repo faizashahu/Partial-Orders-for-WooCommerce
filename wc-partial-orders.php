5fd67f7986e9b56ebcecc07c7411db4f7169c8a0
Farjana Faiza <farjanafaija07@gmail.com>
2025-11-30 02:54:49 +0600

    Updated wc-partial-orders.php

--- wc-partial-orders.php
+++ wc-partial-orders.php
@@ -238,7 +238,7 @@ class WC_Partial_Orders {
             'wc-partial-orders',
             plugins_url('js/partial-orders.js', __FILE__),
             ['jquery'],
-            '1.0.2',
+            '2.0.0',
             true
         );
 
